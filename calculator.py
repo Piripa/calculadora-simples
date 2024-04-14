9fe236aeee071088f25ec2961c511269f285e943
Vinicius <viniaoliveira22@gmail.com>
2024-04-13 23:39:22 -0300

    add new branch and new testes

--- calculator.py
+++ calculator.py
@@ -1,16 +1,15 @@
 class Calculator:
-
     def __init__(self,valor1,valor2):
         self.valor1 = valor1
         self.valor2 = valor2
 
     def soma(valor1, valor2):
-        return 0
+        return valor1 + valor2
     def subtracao(valor1, valor2):
-        return 0
+        return valor1 - valor2
 
     def divisao(valor1, valor2):
-        return 0
+        return valor1 * valor2
     
     def multiplicacao(valor1, valor2):
-        return 0
+        return valor1 / valor2
--- tests/tests_calculator.py
+++ tests/tests_calculator.py
@@ -1,10 +1,10 @@
 import unittest
 from calculator import Calculator
-
+#python -m unittest tests/tests_calculator.py
 class Test_Calculator(unittest.TestCase):
-    def test_soma(self):
-        valor1, valor2 = Calculator(1,2)
-        self.assertEqual(3,Calculator.soma(valor1,valor2))
-   
+    def test_soma1(self):
+        self.assertEqual(3,Calculator.soma(1,2))
+    def test_soma2(self):
+        self.assertEqual(5, Calculator.soma(10,-5))
 if __name__ == "__main__":
     unittest.main()
